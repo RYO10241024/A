02c35b862b00df19d8c95af31a46256b95bfa2eb
RYO10241024 <enomotojapan10@gmail.com>
2025-04-19 00:50:38 +0900

    Update mexc_price_monitor.py

--- mexc_price_monitor.py
+++ mexc_price_monitor.py
@@ -5,7 +5,7 @@ import requests
 
 # トークン設定
 TOKENS = os.getenv("TOKENS", "BTC/USDT,ETH/USDT,XRP/USDT,DOGE/USDT").split(",")
-ALERT_THRESHOLD = float(os.getenv("ALERT_THRESHOLD", "0.01"))
+ALERT_THRESHOLD = float(os.getenv("ALERT_THRESHOLD", "10"))
 
 # MEXCのccxtインスタンス
 exchange = ccxt.mexc()
